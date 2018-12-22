7be2371f170d1fe02e1888c0f9d42e6713b8ec8f
egt 2018 <egt@egts-iMac-3.local>
2018-12-21 16:43:12 -0800

    added tracklist.py

---
+++ Tracklist.py
@@ -0,0 +1,27 @@
+#Imports
+#Call from terminal with $ python Tracklist.py
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome()
+
+driver.get('https://open.spotify.com/browse/featured')
+wait = WebDriverWait(driver, 20)
+element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'btn-black')))
+element.click()
+
+wait = WebDriverWait(driver, 20)
+element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'btn-block')))
+element.click()
+
+wait = WebDriverWait(driver, 20)
+driver.find_element_by_id("email").send_keys('6506783895')
+driver.find_element_by_id("pass").send_keys('ffb420')
+driver.find_element_by_id("loginbutton").click()
+
+
+
+#button = driver.find_element_by_class_name('btn-white').click()
