d747d5205ef73f31a8b86214bb6cc38883c7b357
egt 2018 <egt@egts-iMac-3.local>
2019-01-09 13:50:43 -0800

    added new file for work data analysis

---
+++ data_project.py
@@ -0,0 +1,11 @@
+'''Project to look at todos from corrigo and try to find KPIs'''
+
+import pandas
+
+df = pandas.read_csv("../downloads/test2.csv")
+
+created_list #list of creation dates
+
+#this prints out all values at column pos 3 for all rows in the dataframe
+for x in range(len(df2)):
+    created_list.append(df2.iloc[x,3])
